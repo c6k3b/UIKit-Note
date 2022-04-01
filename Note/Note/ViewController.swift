6a1faec442d1ac99743c0e03b57a5ef98ed0fba4
AA <c6k3b@icloud.com>
2022-04-01 11:13:20 +0400

    refactored

--- Note/Note/ViewController.swift
+++ Note/Note/ViewController.swift
@@ -5,19 +5,24 @@ class ViewController: UIViewController {
     private var textField = UITextField()
     private var textView = UITextView()
     private var isEditingMode = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         navigationItem.title = "Заметка"
         view.backgroundColor = .systemBackground
+
         setupRightBarButton()
         setupTextField()
         setupTextView()
         didRightBarButtonTapped(rightBarButton)
     }
+
     @objc
     private func didRightBarButtonTapped(_ button: UIBarButtonItem) {
         isEditingMode = !isEditingMode
         setUserInteractionState()
+
         if isEditingMode {
             rightBarButton.title = "Готово"
             textView.becomeFirstResponder()
@@ -26,17 +31,21 @@ class ViewController: UIViewController {
             textView.resignFirstResponder()
         }
     }
+
     private func setupRightBarButton() {
         rightBarButton.title = "Изменить"
         rightBarButton.target = self
         rightBarButton.action = #selector(didRightBarButtonTapped(_:))
         navigationItem.rightBarButtonItem = rightBarButton
     }
+
     private func setupTextField() {
         textField.placeholder = "Title"
         textField.font = .systemFont(ofSize: 22, weight: .bold)
         textField.layer.sublayerTransform = CATransform3DMakeTranslation(8, 0, 0)
+
         view.addSubview(textField)
+
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.topAnchor.constraint(
             equalTo: view.safeAreaLayoutGuide.topAnchor,
@@ -51,10 +60,13 @@ class ViewController: UIViewController {
             constant: -16
         ).isActive = true
     }
+
     private func setupTextView() {
         textView.textContainer.lineFragmentPadding = 8
         textView.font = .systemFont(ofSize: 14)
+
         view.addSubview(textView)
+
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.topAnchor.constraint(
             equalTo: textField.bottomAnchor,
@@ -68,10 +80,12 @@ class ViewController: UIViewController {
             equalTo: view.safeAreaLayoutGuide.trailingAnchor,
             constant: -16
         ).isActive = true
-        textView.heightAnchor.constraint(
-            lessThanOrEqualToConstant: 80
+        textView.bottomAnchor.constraint(
+            equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+            constant: 8
         ).isActive = true
     }
+
     private func setUserInteractionState() {
         textField.isUserInteractionEnabled = isEditingMode
         textView.isUserInteractionEnabled = isEditingMode
