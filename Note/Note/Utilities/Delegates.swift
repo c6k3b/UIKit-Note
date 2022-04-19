4cba3c6460e536e01785d22f85e1a976f1a8796a
AA <c6k3b@icloud.com>
2022-04-20 00:13:00 +0400

    closure added to ListVC

---
+++ Note/Note/Utilities/Delegates.swift
@@ -0,0 +1,8 @@
+//
+//  Delegates.swift
+//  Note
+//
+//  Created by aa on 19.04.2022.
+//
+
+import Foundation
--- Note/Note/Utilities/Protocols.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol NoteDelegate: AnyObject {
-    func passNote(_ note: Note)
-}
-
-protocol ShowingNoteDelegate: AnyObject {
-    func showNoteVC(for noteView: NoteView)
-}
