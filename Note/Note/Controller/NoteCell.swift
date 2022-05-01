f0232153ad42a71cda7960b38749f06b62a7866e
AA <c6k3b@icloud.com>
2022-05-01 12:04:42 +0400

    small ui fixes

--- Note/Note/Controller/NoteCell.swift
+++ Note/Note/Controller/NoteCell.swift
@@ -5,14 +5,13 @@ class NoteCell: UITableViewCell, ConfigurableCell {
     static var identifier = String(describing: NoteCell.self)
 
     func configure(with model: ConfigurationModel) {
-        let noteView = NoteCellView(
+        backgroundView = NoteCellView(
             model: NoteCellView.Model(
                 header: model.header ?? "N/A",
                 body: model.body ?? "N/A",
                 date: model.date.getFormattedDate(format: "dd.MM.yyyy")
             ), frame: .infinite
         )
-        contentView.addSubview(noteView)
         selectionStyle = .none
     }
 }
