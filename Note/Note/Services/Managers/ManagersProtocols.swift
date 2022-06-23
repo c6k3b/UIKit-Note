c14435c921fbaeb35b31001e3b5f1eef8c6271ce
AA <c6k3b@icloud.com>
2022-06-24 00:09:32 +0400

    rm

--- Note/Note/Services/Managers/ManagersProtocols.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-protocol NetworkProtocol {
-    func fetchData(completion: ([NoteData]) -> Void)
-    func fetchImage(from stringUrl: String) -> Data?
-}
--- Note/Note/Services/ServicesProtocols.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-protocol NotesStoreProtocol {
-    func remove(_ index: [Int]?, completion: (([Note]?) -> Void))
-    func update(completion: (([Note]?) -> Void))
-}
